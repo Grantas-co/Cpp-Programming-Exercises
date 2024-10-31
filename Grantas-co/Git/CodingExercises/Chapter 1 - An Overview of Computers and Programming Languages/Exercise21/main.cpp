fd0a510bc7c3807cd00396b2878e0a043db09703
Grantas-co <info@grantas.co>
2024-10-31 11:26:57 +0100

    Delete Grantas-co directory

--- Grantas-co/Git/CodingExercises/Chapter 1 - An Overview of Computers and Programming Languages/Exercise21/main.cpp
+++
@@ -1,5 +0,0 @@
-// ===================== October 14th 2024 - Grantas.co ===============================================
-// ============================== Exercise 21 =========================================================
-// Design an algorithm to find the real roots of a quadratic equation of the form 
-// ax^2 + bx + c = 0, where a, b, and c are real numbers, and a is nonzero. (9)
-// ====================================================================================================
--- Grantas-co/Git/CodingExercises/Chapter 1 - An Overview of Computers and Programming Languages/Exercise22/main.cpp
+++
@@ -1,8 +0,0 @@
-// ===================== October 14th 2024 - Grantas.co ===============================================
-// ============================== Exercise 22 =========================================================
-// A student spends a majority of his weekend playing and watching sports, thereby tiring him out and 
-// leading him to oversleep and often miss his Monday 8 AM math class. Suppose that the tuition per 
-// semester is $25,000 and the average semester consists of 15 units. If the math class meets three 
-// days a week, one hour each day for 15 weeks, and is a four-unit course, how much does each hour of
-// math class cost the student? Design an algorithm that computes the cost of each math class.
-// ====================================================================================================
